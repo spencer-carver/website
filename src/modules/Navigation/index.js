a872b83d489e5dea54bf7ceb80eb7501e2b6af7f
Spencer Carver <spencer.carver@gmail.com>
2018-10-06 14:27:53 -0400

    [SC] [Untracked] Update Nav formatting

--- src/modules/Navigation/index.js
+++ src/modules/Navigation/index.js
@@ -1,20 +1,3 @@
-import React from "react";
-import "./styles.css";
-
-const Navigation = () => {
-    return (
-        <nav className="navigation">
-            <div className="spacer-left"></div>
-            <div className="site-title"><h1>Currently Under Construction</h1></div>
-            <div className="social-links desktop">
-                <a className="social-icon facebook" href="https://www.facebook.com/spencerrc" target="_blank" rel="noopener noreferrer">Facebook</a>
-                <a className="social-icon twitter" href="https://twitter.com/spencerrc" target="_blank" rel="noopener noreferrer">Twitter</a>
-                <a className="social-icon github" href="https://github.com/spencer-carver" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a className="social-icon linked-in" href="https://www.linkedin.com/in/spencerrc" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            </div>
-            <div className="social-links mobile"></div>
-        </nav>
-    );
-};
+import Navigation from "./component.js";
 
 export default Navigation;
