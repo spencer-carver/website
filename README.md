6939e52fe0a77fcb9121ec0e8341ef84ad0e311b
Spencer Carver <spencer.carver@gmail.com>
2018-10-06 14:44:52 -0400

    [SC] [Untracked] add README and some more mobile nav tweaks

---
+++ README.md
@@ -0,0 +1,4 @@
+This repository houses the code running my personal website at https://spencer.carvers.info. I hope to use it to showcase my prefered coding styles, and also to keep learning about the latest JS and Front-End development trends.
+
+##Known Issues
+* Social Icons don't appear in IE11 (SVG data URLs inlined into CSS)
--- src/modules/Navigation/component.js
+++ src/modules/Navigation/component.js
@@ -12,13 +12,12 @@ const Navigation = () => {
         <nav className="navigation">
             <div className="spacer-left"></div>
             <div className="site-title"><h1>Currently Under Construction</h1></div>
-            <div className="social-links desktop">
+            <div className="social-links">
                 <a className="social-icon facebook" href={ FACEBOOK_URL } target="_blank" rel="noopener noreferrer">Facebook</a>
                 <a className="social-icon twitter" href={ TWITTER_URL } target="_blank" rel="noopener noreferrer">Twitter</a>
                 <a className="social-icon github" href={ GITHUB_URL } target="_blank" rel="noopener noreferrer">GitHub</a>
                 <a className="social-icon linked-in" href={ LINKEDIN_URL } target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
-            <div className="social-links mobile"></div>
         </nav>
     );
 };
