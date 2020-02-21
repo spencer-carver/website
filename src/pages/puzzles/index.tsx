12102915510a032426f42c6449ed89fe809b173a
Spencer Carver <spencer.carver@gmail.com>
2020-02-20 23:29:38 -0500

    Add 'Judge Calls' Puzzle (#40)

--- src/pages/puzzles/index.tsx
+++ src/pages/puzzles/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../../modules/Navigation";
+import { Puzzle } from "../puzzle";
 import puzzles from "../puzzle/puzzles.json";
 import styles from "../puzzle/styles.module.scss";
 
@@ -13,8 +14,10 @@ const Puzzles = (): JSX.Element => {
                     I&apos;m trying to make puzzles! Here&apos;s all my attempts in order of creation. This page is not a puzzle.
                 </div>
                 <ul className={ styles.list }>
-                    { Object.keys(puzzles).map((puzzleName, index) => {
-                        return <li key={ index }><Link to={ `/puzzle/${ puzzleName }` }>{ puzzleName }</Link></li>;
+                    { Object.keys(puzzles).map((puzzleId: string, index: number) => {
+                        const puzzle = (puzzles as { [key: string]: Puzzle })[puzzleId];
+
+                        return <li key={ index }><Link to={ `/puzzle/${ puzzleId }` }>{ puzzle.title }</Link></li>;
                     }) }
                 </ul>
             </div>
