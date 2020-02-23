3cb82e4c59d51d91339cc4b6d6133d5994424633
Spencer Carver <spencer.carver@gmail.com>
2020-02-23 14:42:40 -0500

    Revamp Slideshow and Projects sections, some additional cleanup (#49)

--- src/modules/Slideshow/index.tsx
+++ src/modules/Slideshow/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import backgroundImage from "../../images/bg.jpg";
+import { FunctionalComponent, PropsObject } from "../../constants/Types";
 import styles from "./styles.module.scss";
 
 const TRANSITION_TIME = 8000; // in ms
@@ -9,36 +11,56 @@ declare global {
     }
 }
 
-interface SlideshowProps {
-    images: Array<string>;
-    isHero: boolean;
+interface SlideshowOptions {
+    isHero?: boolean;
     overlayLogo?: boolean;
     hideMobile?: boolean;
+    backgroundImage?: string;
 }
 
-const Slideshow = ({ images, isHero, overlayLogo = false, hideMobile = false }: SlideshowProps): JSX.Element => {
+interface SlideshowProps {
+    items: Array<PropsObject>;
+    component: FunctionalComponent;
+    options: SlideshowOptions;
+}
+
+const Slideshow = ({ items, component: Component, options }: SlideshowProps): JSX.Element => {
+    const {
+        isHero = false,
+        overlayLogo = false,
+        hideMobile = false
+    } = options;
+
     const [ selected, setSelected ] = useState(0);
 
-    function nextPhoto(): void {
-        const newIndex = selected === images.length - 1
+    function nextItem(): void {
+        const newIndex = selected === items.length - 1
             ? 0
             : selected + 1;
 
         setSelected(newIndex);
 
+        if (!isHero) {
+            return;
+        }
+
         clearInterval(window.slideshowTimer);
-        window.slideshowTimer = setInterval(nextPhoto, 2 * TRANSITION_TIME);
+        window.slideshowTimer = setInterval(nextItem, 2 * TRANSITION_TIME);
     }
 
-    function previousPhoto(): void {
+    function previousItem(): void {
         const newIndex = selected === 0
-            ? images.length - 1
+            ? items.length - 1
             : selected - 1;
 
         setSelected(newIndex);
 
+        if (!isHero) {
+            return;
+        }
+
         clearInterval(window.slideshowTimer);
-        window.slideshowTimer = setInterval(previousPhoto, 2 * TRANSITION_TIME);
+        window.slideshowTimer = setInterval(previousItem, 2 * TRANSITION_TIME);
     }
 
     useEffect(() => {
@@ -46,43 +68,55 @@ const Slideshow = ({ images, isHero, overlayLogo = false, hideMobile = false }: 
             return;
         }
 
-        window.slideshowTimer = setInterval(nextPhoto, TRANSITION_TIME);
+        window.slideshowTimer = setInterval(nextItem, TRANSITION_TIME);
 
         return (): void => clearInterval(window.slideshowTimer);
     });
 
     return (
-        <div className={ isHero ? "" : "section" }>
+        <div>
             <div className={ `${ isHero ? styles.hero : "" } ${ styles.container } ${ overlayLogo ? styles.overlayLogo : "" }` }>
                 <div className={ `${ isHero ? styles.hero : "" } ${ styles.slideshow } ${ hideMobile ? styles.hideMobile : "" }` }>
-                    {
-                        images.map((image, index) => {
-                            return (
-                                <div key={ index } className={ `${ styles.slide } ${ selected === index ? styles.selected : "" }` }>
-                                    <img src={ image } className={ styles.image } alt="Spencer" />
-                                </div>
-                            );
-                        })
-                    }
-                    <img src={ images[0] } className={ `${ styles.image } ${ styles.placeholder }` } alt="Spencer" />
+                    <img src={ backgroundImage } className={ `${ styles.image } ${ styles.placeholder }` } alt="background" />
+                    { items.map((props, index) => <Component { ...props } key={ index } index={ index } selected={ selected } />) }
                     <div className={ styles.indicators }>
-                        {
-                            images.map((image, index) => {
-                                return (
-                                    <div key={ index } className={ `${ styles.indicator } ${ selected === index ? styles.selected : "" }` }
-                                        onClick={ (): void => setSelected(index) }>
-                                        <span className={ styles.indicatorText }>{ index + 1 }</span>
-                                    </div>
-                                );
-                            })
-                        }
+                        { items.map((item, index) => <Indicator key={ index } index={ index } selected={ selected } setSelected={ setSelected } />) }
                     </div>
-                    <div className={ styles.prev } onClick={ previousPhoto }>&#10094;</div>
-                    <div className={ styles.next } onClick={ nextPhoto }>&#10095;</div>
+                    <div className={ styles.prev } onClick={ previousItem }>&#10094;</div>
+                    <div className={ styles.next } onClick={ nextItem }>&#10095;</div>
                 </div>
             </div>
         </div>
         
+    );
+};
+
+interface IndicatorProps {
+    index: number;
+    selected: number;
+    setSelected: Function;
+}
+
+const Indicator = ({ index, selected, setSelected }: IndicatorProps): JSX.Element => {
+    return (
+        <div className={ `${ styles.indicator } ${ selected === index ? styles.selected : "" }` }
+            onClick={ (): void => setSelected(index) }>
+            <span className={ styles.indicatorText }>{ index + 1 }</span>
+        </div>
+    );
+};
+
+interface ImageSlideProps {
+    index: number;
+    selected: number;
+    image: string;
+}
+
+export const ImageSlide = ({ index, image, selected }: ImageSlideProps): JSX.Element => {
+    return (
+        <div className={ `${ styles.slide } ${ selected === index ? styles.selected : "" }` }>
+            <img src={ image } className={ styles.image } alt="Spencer" />
+        </div>
     );
 };
 
