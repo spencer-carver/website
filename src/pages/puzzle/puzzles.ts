66d528d5b3d1de02e1586140f2cd03260ea3686d
Spencer Carver <spencer.carver@gmail.com>
2020-02-25 22:37:12 -0500

    rename puzzle assets

--- src/pages/puzzle/puzzles.ts
+++ src/pages/puzzle/puzzles.ts
@@ -15,13 +15,13 @@ const PUZZLES: { [key: string]: Puzzle } = {
         title: "Tetris",
         type: PuzzleType.video,
         description: "You don't see the appeal?",
-        srcUrl: "https://spencer.carvers.info/static/media/tetris-puzzle.mp4"
+        srcUrl: "https://spencer.carvers.info/static/puzzle/tetris.mp4"
     },
     "judge-calls-one": {
         title: "Judge Calls",
         type: PuzzleType.pdf,
         description: "Can you decrypt what the players are talking about?",
-        srcUrl: "https://spencer.carvers.info/static/media/judge-calls-one.pdf"
+        srcUrl: "https://spencer.carvers.info/static/puzzle/judge-calls-one.pdf"
     }
 };
 
