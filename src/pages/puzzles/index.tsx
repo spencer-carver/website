1fe14b04a2a7f12092ad410a2a217dadeecbf3f2
Spencer Carver <spencer.carver@gmail.com>
2020-02-29 20:39:19 -0500

    mobile answer badge and cookie clear logic

--- src/pages/puzzles/index.tsx
+++ src/pages/puzzles/index.tsx
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../../modules/Navigation";
 import puzzles, { Puzzle } from "../../puzzles";
 import getCookieValue from "../../utils/getCookieValue";
+import { PuzzleComplete } from "../puzzle";
 import styles from "../puzzle/styles.module.scss";
 
 const Puzzles = (): JSX.Element => {
+    const answerCount = Object.keys(puzzles).reduce((count: number, puzzleId: string): number => count + (getCookieValue(puzzleId) ? 1 : 0), 0);
+    const [ numberAnswered, setNumberAnswered ] = useState(answerCount);
+
+    const clearAnswer = (puzzleId: string): void => {
+        document.cookie = `${ puzzleId }=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+        setNumberAnswered(numberAnswered - 1);
+    };
+
     return (
         <Navigation isHomepage={ false }>
+            <PuzzleComplete answer={ `SOLVED: ${ numberAnswered }` } />
             <div className={ styles.puzzle }>
                 <div className={ styles.title }>Puzzles</div>
-                <div className={ styles.description }>
+                <p className={ styles.description }>
                     I&apos;m trying to make puzzles! Here are all of my attempts. This page is not a puzzle.
-                </div>
+                </p>
                 <ul className={ styles.list }>
                     { Object.keys(puzzles).map((puzzleId: string, index: number) => {
                         const puzzle = (puzzles as { [key: string]: Puzzle })[puzzleId];
 
                         const puzzleAnswer = getCookieValue(puzzleId);
+                        const clearPuzzleAnswer = (): void => clearAnswer(puzzleId);
 
                         return (
                             <li key={ index }>
                                 <Link to={ `/puzzle/${ puzzleId }` }>{ puzzle.title }</Link>
-                                { puzzleAnswer && <span className={ styles.answer }>{ puzzleAnswer }</span> }
+                                { puzzleAnswer && (
+                                    <span className={ styles.answer }
+                                        role="button"
+                                        tabIndex={ 0 }
+                                        title="Clear Answer"
+                                        onKeyPress={ clearPuzzleAnswer }
+                                        onClick={ clearPuzzleAnswer }
+                                    >
+                                        { puzzleAnswer }
+                                    </span> 
+                                ) }
                             </li>
                         );
                     }) }
                 </ul>
+                { numberAnswered > 0 && (
+                    <p className={ styles.description }>
+                        If you want to erase an answer (perhaps to let someone else try the puzzle), just click on it!
+                    </p>
+                ) }
             </div>
         </Navigation>
     );
