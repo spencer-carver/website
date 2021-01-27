86a756a17deefbeb44292f6adf5fb766a5df7a92
Spencer Carver <spencer.carver@gmail.com>
2021-01-26 22:05:22 -0500

    Dark Theme (#98)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import "react-app-polyfill/ie11";
-import React, { FunctionComponent } from "react";
+import React, { useState, FunctionComponent } from "react";
 import { render } from "react-snapshot";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Footer from "./modules/Footer";
@@ -15,28 +15,49 @@ import Error from "./pages/error";
 import { unregister as unregisterServiceWorker } from "./registerServiceWorker";
 import "./index.scss";
 
-const Sitemap: FunctionComponent = () => (
-    <Router>
-        <main className="page">
-            <Switch>
-                <Route path="/" exact component={ Homepage } />
-                <Redirect from="/puzzle" exact to="/puzzles" />
-                <Route path="/puzzles" exact component={ Puzzles } />
-                <Route path="/puzzle/:puzzleName" exact component={ Puzzle } />
-                <Redirect from="/cocktail" exact to="/cocktails" />
-                <Route path="/cocktails" exact component={ Cocktails } />
-                <Redirect from="/recipe" exact to="/recipes" />
-                <Route path="/recipes" exact component={ Recipes } />
-                <Route path="/recipe/:recipeName" exact component={ Recipe } />
-                <Redirect from="/mtg" exact to="/magic" />
-                <Route path="/magic" exact component={ Magic } />
-                <Route path="/magic/deck/:deckName" exact component={ MagicDeck } />
-                <Route path="*" render={ (() => <Error errorCode={ 404 } />) as FunctionComponent } />
-            </Switch>
-            <Footer />
-        </main>
-    </Router>
-);
+const getDefaultTheme = (): string => {
+    let theme;
+
+    try {
+        theme = localStorage.getItem("theme");
+    } catch (e) {
+        // do nothing
+    }
+
+    return theme || "light";
+};
+
+const Sitemap: FunctionComponent = () => {
+    const [ theme, setTheme ] = useState(getDefaultTheme());
+
+    const saveTheme = (theme: string): void => {
+        localStorage.setItem("theme", theme);
+        setTheme(theme);
+    };
+    
+    return (
+        <Router>
+            <main className="page" data-theme={ theme }>
+                <Switch>
+                    <Route path="/" exact component={ Homepage } />
+                    <Redirect from="/puzzle" exact to="/puzzles" />
+                    <Route path="/puzzles" exact component={ Puzzles } />
+                    <Route path="/puzzle/:puzzleName" exact component={ Puzzle } />
+                    <Redirect from="/cocktail" exact to="/cocktails" />
+                    <Route path="/cocktails" exact component={ Cocktails } />
+                    <Redirect from="/recipe" exact to="/recipes" />
+                    <Route path="/recipes" exact component={ Recipes } />
+                    <Route path="/recipe/:recipeName" exact component={ Recipe } />
+                    <Redirect from="/mtg" exact to="/magic" />
+                    <Route path="/magic" exact component={ Magic } />
+                    <Route path="/magic/deck/:deckName" exact component={ MagicDeck } />
+                    <Route path="*" render={ (() => <Error errorCode={ 404 } />) as FunctionComponent } />
+                </Switch>
+                <Footer setTheme={ saveTheme }/>
+            </main>
+        </Router>
+    );
+};
 
 render(<Sitemap />, document.getElementById("root"));
 unregisterServiceWorker();
--- src/modules/Footer/index.tsx
+++ src/modules/Footer/index.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from "react";
+import Theme from "../Theme";
 import styles from "./styles.module.scss";
 
-const Footer: FunctionComponent = () => {
+const Footer: FunctionComponent<{ setTheme: Function }> = ({ setTheme }) => {
     return (
-        <div className={ styles.spacer }>
-            <footer className={ styles.footer }>
-                <p className={ styles.copyrightNotice }>&#169; 2019-{ (new Date()).getFullYear() } Spencer Carver</p>
-            </footer>
-        </div>
+        <>
+            <div className={ styles.spacer }>
+                <footer className={ styles.footer }>
+                    <Theme setTheme={ setTheme } />
+                    <p className={ styles.copyrightNotice }>&#169; 2019-{ (new Date()).getFullYear() } Spencer Carver</p>
+                </footer>
+            </div>
+        </>
     );
 };
 
